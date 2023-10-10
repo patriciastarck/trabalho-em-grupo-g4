40dd86a0f330d47f128e0f8ab79c0af344e83301
paulotulerfaeterj <paulogustavotuler@gmail.com>
2023-10-09 22:09:11 -0300

    commit inicial

---
+++ grupo4/src/main/java/br/com/api/g4/Grupo4Application.java
@@ -0,0 +1,13 @@
+package br.com.api.g4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo4Application.class, args);
+	}
+
+}
---
+++ grupo4/src/test/java/br/com/api/g4/Grupo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.g4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
