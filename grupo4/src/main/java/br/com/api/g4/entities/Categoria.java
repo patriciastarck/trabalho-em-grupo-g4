d6c6e25d4d9267eb2015d0afa1dca5d7afc16d68
paulotulerfaeterj <paulogustavotuler@gmail.com>
2023-10-11 18:20:32 -0300

    entity categoria

---
+++ grupo4/src/main/java/br/com/api/g4/entities/Categoria.java
@@ -0,0 +1,40 @@
+package br.com.api.g4.entities;
+
+public class Categoria {
+
+	private Integer id;
+	private String nome;
+	private String descricao;
+	public Categoria() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Categoria(Integer id, String nome, String descricao) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	@Override
+	public String toString() {
+		return "Categoria [id=" + id + ", nome=" + nome + ", descricao=" + descricao + "]";
+	}
+}
