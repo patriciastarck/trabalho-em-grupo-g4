3a84a3024efa187228df9e81d85f48673b8870dc
paulotulerfaeterj <paulogustavotuler@gmail.com>
2023-10-11 18:01:58 -0300

    entity produto

---
+++ grupo4/src/main/java/br/com/api/g4/entities/Produto.java
@@ -0,0 +1,84 @@
+package br.com.api.g4.entities;
+
+import java.time.LocalDate;
+
+public class Produto {
+
+	private Integer id;
+	private String nome;
+	private String descricao;
+	private LocalDate data_fabricacao;
+	private Integer qntd_estoque;
+	private Double valor_unitario;
+
+	public Produto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDate getData_fabricacao() {
+		return data_fabricacao;
+	}
+
+	public void setData_fabricacao(LocalDate data_fabricacao) {
+		this.data_fabricacao = data_fabricacao;
+	}
+
+	public Integer getQntd_estoque() {
+		return qntd_estoque;
+	}
+
+	public void setQntd_estoque(Integer qntd_estoque) {
+		this.qntd_estoque = qntd_estoque;
+	}
+
+	public Double getValor_unitario() {
+		return valor_unitario;
+	}
+
+	public void setValor_unitario(Double valor_unitario) {
+		this.valor_unitario = valor_unitario;
+	}
+
+	public Produto(Integer id, String nome, String descricao, LocalDate data_fabricacao, Integer qntd_estoque,
+			Double valor_unitario) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.data_fabricacao = data_fabricacao;
+		this.qntd_estoque = qntd_estoque;
+		this.valor_unitario = valor_unitario;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", nome=" + nome + ", descricao=" + descricao + ", data_fabricacao="
+				+ data_fabricacao + ", qntd_estoque=" + qntd_estoque + ", valor_unitario=" + valor_unitario + "]";
+	}
+
+}
--- grupo4/src/main/java/br/com/api/g4/entities/Usuario.java
+++ grupo4/src/main/java/br/com/api/g4/entities/Usuario.java
@@ -9,16 +9,96 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="usuario")
+@Table(name = "usuario")
 public class Usuario {
 
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)//informa q é pk
-		private Integer id;
-		private String nome;
-		private Boolean endereco;
-		private String nome_usuario;
-		private String email;
-		private String cpf;
-		private LocalDate data_nascimento;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // informa q é pk
+	private Integer id;
+	private String nome;
+	private Boolean endereco;
+	private String nome_usuario;
+	private String email;
+	private String cpf;
+	private LocalDate data_nascimento;
+
+	public Usuario() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nome=" + nome + ", endereco=" + endereco + ", nome_usuario=" + nome_usuario
+				+ ", email=" + email + ", cpf=" + cpf + ", data_nascimento=" + data_nascimento + "]";
+	}
+
+	public Usuario(Integer id, String nome, Boolean endereco, String nome_usuario, String email, String cpf,
+			LocalDate data_nascimento) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.endereco = endereco;
+		this.nome_usuario = nome_usuario;
+		this.email = email;
+		this.cpf = cpf;
+		this.data_nascimento = data_nascimento;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Boolean getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Boolean endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getNome_usuario() {
+		return nome_usuario;
+	}
+
+	public void setNome_usuario(String nome_usuario) {
+		this.nome_usuario = nome_usuario;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public LocalDate getData_nascimento() {
+		return data_nascimento;
+	}
+
+	public void setData_nascimento(LocalDate data_nascimento) {
+		this.data_nascimento = data_nascimento;
+	}
+
 }
