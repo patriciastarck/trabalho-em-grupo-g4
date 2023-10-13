b333b433a5c944961a7ae29d2aa9f5ce2a4c2942
Yslânio victor <dev.yslanio@gmail.com>
2023-10-13 18:36:03 -0300

    build: repository

--- grupo4/src/main/java/br/com/api/g4/repositories/PedidoRepository.java
+++ grupo4/src/main/java/br/com/api/g4/repositories/PedidoRepository.java
@@ -1,6 +1,7 @@
 package br.com.api.g4.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.api.g4.entities.Pedido;
@@ -8,4 +9,6 @@ import br.com.api.g4.entities.Pedido;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido,Integer> {
 	
+	@Query(value="select count(*) from pedido ", nativeQuery=true)
+    public Integer contar();
 }
--- grupo4/src/main/java/br/com/api/g4/repositories/UsuarioRepository.java
+++ grupo4/src/main/java/br/com/api/g4/repositories/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package br.com.api.g4.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.api.g4.entities.Usuario;
@@ -8,5 +9,7 @@ import br.com.api.g4.entities.Usuario;
 	@Repository
 	public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
+		@Query(value="select count(*) from usuario ", nativeQuery=true)
+	    public Integer contar();
 	}
 
