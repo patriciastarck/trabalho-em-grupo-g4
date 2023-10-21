8dc29b2377e67e7b9faf719bce109c3edc751e1e
Ramon Gall <oliveiraagall@gmail.com>
2023-10-21 12:23:57 -0300

    feat:Começo de criação de método disparo do email pedidos.

--- grupo4/src/main/java/br/com/api/g4/services/EmailService.java
+++ grupo4/src/main/java/br/com/api/g4/services/EmailService.java
@@ -3,6 +3,8 @@ package br.com.api.g4.services;
 import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 
@@ -21,6 +23,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.api.g4.dto.PromocaoDTO;
 import br.com.api.g4.dto.UsuarioDTO;
+import br.com.api.g4.entities.Pedido;
 import br.com.api.g4.entities.Usuario;
 
 @Configuration
@@ -31,13 +34,16 @@ public class EmailService {
 	UsuarioService usuarioService;
 	@Autowired
 	ProdutoService produtoService;
-	
+
 	private JavaMailSender emailSender;
 
 	@Autowired
 	public void setJavaMailSender(JavaMailSender emailSender) {
 		this.emailSender = emailSender;
 	}
+
+//	@Autowired
+//	Pedido pedido;
 
 	@Value("${spring.mail.host}")
 	private String host;
@@ -66,69 +72,74 @@ public class EmailService {
 		return emailSender;
 	}
 
-	//TODO terminar metodo e botar no pedido
-	public void envioEmailPedido() {
-		MimeMessage mensagemCadastro = emailSender.createMimeMessage();
-		try {
-			MimeMessageHelper helper = new MimeMessageHelper(mensagemCadastro, true);
-			helper.setFrom("gp4api.serratec@gmail.com");
-			helper.setTo("patriciastarck@gmail.com");
-			helper.setSubject("Me chama que eu vou!!");
-
-			LocalDate localDate = LocalDate.now();
-			DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-			String dataEntrega = localDate.plusDays(7).format(format);
-
-			Double valor = 15.9;
-			DecimalFormat df = new DecimalFormat("R$ ,##0.00");
-
-			StringBuilder builder = new StringBuilder();
-			builder.append("<html>\r\n");
-			builder.append("<body>\r\n");
-			builder.append("<div align=\"center\">\r\n");
-			builder.append("<h1>Convite</h1>\r\n");
-			builder.append("</div>\r\n");
-			builder.append("<br/> \r\n");
-
-			builder.append("<center>");
-			builder.append("<table border='2' cellpadding='4'> \r\n");
-			builder.append("<tr> <th> Nome </th> <\r\n");
-			builder.append("<tr> <th> Nome</th> <th> Email</th> <th>Perfis</th><th> Data de entrega</th> </tr>\r\n");
-
-			List<Usuario> listaUsuarios = usuarioService.listar();
-			for (Usuario usuario : listaUsuarios) {
-				builder.append(" <tr>\r\n");
-				builder.append(" <td>\r\n");
-				builder.append(usuario.getNomeUsuario());
-				builder.append(" </td>\r\n");
-				builder.append(" <td>\r\n");
-				builder.append(usuario.getEmail());
-				builder.append(" </td>\r\n");
-				builder.append(" <td>\r\n");
-				builder.append(" </td>\r\n");
-				builder.append(" <td>\r\n");
-				builder.append(dataEntrega);
-				builder.append(" </td>\r\n");
-
-			}
-			builder.append(" </table>\r\n");
-			builder.append(" </center>\r\n");
-			builder.append(" <table border='1' cellpadding='1' >\r\n");
-			builder.append("<tr><th>Valor Total</th></tr>\r\n");
-			builder.append(" <td>\r\n");
-			builder.append(df.format(valor));
-			builder.append(" </td>\r\n");
-			builder.append(" </table>\r\n");
-			builder.append(" </body>\r\n");
-			builder.append("</html>");
-
-			helper.setText(builder.toString(), true);
-			emailSender.send(mensagemCadastro);
-
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		}
-	}
+	// TODO terminar metodo e botar no pedido
+//	public void envioEmailPedido() {
+//		MimeMessage mensagemCadastro = emailSender.createMimeMessage();
+//		try {
+//			MimeMessageHelper helper = new MimeMessageHelper(mensagemCadastro, true);
+//			helper.setFrom("gp4api.serratec@gmail.com");
+//			helper.setTo("oliveiraagall@gmail.com");
+//			helper.setSubject("seu pedido esta sendo enviado");
+//
+//			LocalDate localDate = LocalDate.now();
+//			DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//			String dataEntrega = localDate.plusDays(7).format(format);
+//			double valorTotal = 0;
+//			List<String> nomes =new ArrayList<>();
+//			List<Double> valores=new ArrayList<>();
+//			for (int i = 0; i < pedido.getProdutos().size(); i++) {
+//				valores.add(pedido.getProdutos().get(i).getValorUnitario());
+//				nomes.add(pedido.getProdutos().get(i).getNome());
+//				valorTotal += pedido.getProdutos().get(i).getValorUnitario();
+//			}
+//			DecimalFormat df = new DecimalFormat("R$ ,##0.00");
+//
+//			StringBuilder builder = new StringBuilder();
+//			builder.append("<html>\r\n");
+//			builder.append("<body>\r\n");
+//			builder.append("<div align=\"center\">\r\n");
+//			builder.append("<h1>Convite</h1>\r\n");
+//			builder.append("</div>\r\n");
+//			builder.append("<br/> \r\n");
+//
+//			builder.append("<center>");
+//			builder.append("<table border='2' cellpadding='4'> \r\n");
+//			builder.append("<tr> <th> Nome </th> <\r\n");
+//			builder.append("<tr> <th> Nome</th> <th> Email</th> <th>Perfis</th><th> Data de entrega</th> </tr>\r\n");
+//
+//			for (int i = 0; i< nomes.size();i++) {
+//				builder.append(dataEntrega);
+//				builder.append(" <tr>\r\n");
+//				builder.append(" <td>\r\n");
+//				builder.append(nomes.get(i));
+//				builder.append(" </td>\r\n");
+//				builder.append(" <td>\r\n");
+//				builder.append(df.format(valores.get(i)));
+//				builder.append(" </td>\r\n");
+//				builder.append(" <td>\r\n");
+//				builder.append(" </td>\r\n");
+//				builder.append(" <td>\r\n");
+//				builder.append(" </td>\r\n");
+//			}
+//			
+//			builder.append(" </table>\r\n");
+//			builder.append(" </center>\r\n");
+//			builder.append(" <table border='1' cellpadding='1' >\r\n");
+//			builder.append("<tr><th>Valor Total</th></tr>\r\n");
+//			builder.append(" <td>\r\n");
+//			builder.append(df.format(valorTotal));
+//			builder.append(" </td>\r\n");
+//			builder.append(" </table>\r\n");
+//			builder.append(" </body>\r\n");
+//			builder.append("</html>");
+//
+//			helper.setText(builder.toString(), true);
+//			emailSender.send(mensagemCadastro);
+//
+//		} catch (MessagingException e) {
+//			e.printStackTrace();
+//		}
+//	}
 
 	public void envioEmailCadastro(UsuarioDTO objetousuario) {
 		MimeMessage mensagemCadastro = emailSender.createMimeMessage();
@@ -155,7 +166,8 @@ public class EmailService {
 			builder.append("<div align=\"center\">\r\n");
 			builder.append("<p>Parabéns " + nome + " por agora fazer parte do melhor marketplace do mundo!!!</p>");
 			builder.append("<p>Esperamos que tenha uma boa experiência conosco.</p>");
-			builder.append("<a href=http:\"//localhost:8080/api/swagger-ui/index.html#/\"\"\">Clique aqui para voltar ao site </a>\r\n");
+			builder.append(
+					"<a href=http:\"//localhost:8080/api/swagger-ui/index.html#/\"\"\">Clique aqui para voltar ao site </a>\r\n");
 			builder.append("<p>Atenciosamente Grupo 4.\r\n</p>");
 			builder.append("");
 			builder.append("</div>\r\n");
@@ -173,7 +185,7 @@ public class EmailService {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void envioEmailDelete(Usuario usuario) {
 		MimeMessage mensagemCadastro = emailSender.createMimeMessage();
 
@@ -185,26 +197,13 @@ public class EmailService {
 			helper.setSubject("Olá " + nome + " sua conta foi apagada com sucesso.");
 
 			StringBuilder builder = new StringBuilder();
-			builder.append("<html>\r\n" 
-					+ "<body>\r\n" 
-					+ "" 
-					+ "<div align=\"center\">\r\n"
-					+ "<h1>Conta apagada com sucesso</h1>\r\n" 
-					+ "</div>\r\n" 
-					+ "<br/>\r\n" 
-					+ ""
-					+ "<div align=\"center\">\r\n" 
-					+ "<img src=\"cid:logo\">" 
-					+ "</div>\r\n"
-					+ ""
-					+ "<div align=\"center\">\r\n" 
-					+ "" 
+			builder.append("<html>\r\n" + "<body>\r\n" + "" + "<div align=\"center\">\r\n"
+					+ "<h1>Conta apagada com sucesso</h1>\r\n" + "</div>\r\n" + "<br/>\r\n" + ""
+					+ "<div align=\"center\">\r\n" + "<img src=\"cid:logo\">" + "</div>\r\n" + ""
+					+ "<div align=\"center\">\r\n" + ""
 					+ "<p>Agradecemos por utilizar nossos serviços, sua conta foi finalizada.</p>"
-					+ "<p>Esperamos que você tenha tido uma boa experiência conosco! Até a próxima ;)</p>" 
-					+ "<p>Atenciosamente Grupo 4.</p>" 
-					+ "</div>" 
-					+ "</body>\r\n"
-					+ "</html>\r\n");
+					+ "<p>Esperamos que você tenha tido uma boa experiência conosco! Até a próxima ;)</p>"
+					+ "<p>Atenciosamente Grupo 4.</p>" + "</div>" + "</body>\r\n" + "</html>\r\n");
 
 			helper.setText(builder.toString(), true);
 
@@ -226,35 +225,22 @@ public class EmailService {
 			helper.setFrom("gp4api.serratec@gmail.com");
 			helper.setTo("oliveiraagall@gmail.com");
 			helper.setSubject("Olá, veja os produtos em promoção essa semana.");
-			
+
 			StringBuilder builder = new StringBuilder();
-			builder.append("<html>\r\n" 
-					+ "<body>\r\n" 
-					+ "" 
-					+ "<div align=\"center\">\r\n"
-					+ "<h1>Os 5 produtos da semana são</h1>\r\n" 
-					+ "</div>\r\n" + "<br/>\r\n" 
-					+ ""
-					+ "<div align=\"center\">\r\n" 
-					+ "<img src=\"cid:logo\">" 
-					+ "</div>\r\n"
-					+"<div align=\"center\">\r\n"); 
-				 	List<PromocaoDTO> promo = produtoService.promocao();
-				 	for (int i = 0 ; i <promo.size();i++ ) {
-				 		builder.append("<p> produto:" + promo.get(i).getNome()+" - \r\n"); 
-				 		builder.append("valor: R$"+promo.get(i).getValorUnitario()+"</p> \r\n");
-					}
-				 	
-					builder.append(
-					"</div>" 
-					+ "<div align=\"center\">\r\n" 
-					+ "<p>Não perca essas ofertas que estão no site.</p>"
+			builder.append("<html>\r\n" + "<body>\r\n" + "" + "<div align=\"center\">\r\n"
+					+ "<h1>Os 5 produtos da semana são</h1>\r\n" + "</div>\r\n" + "<br/>\r\n" + ""
+					+ "<div align=\"center\">\r\n" + "<img src=\"cid:logo\">" + "</div>\r\n"
+					+ "<div align=\"center\">\r\n");
+			List<PromocaoDTO> promo = produtoService.promocao();
+			for (int i = 0; i < promo.size(); i++) {
+				builder.append("<p> produto:" + promo.get(i).getNome() + " - \r\n");
+				builder.append("valor: R$" + promo.get(i).getValorUnitario() + "</p> \r\n");
+			}
+
+			builder.append("</div>" + "<div align=\"center\">\r\n" + "<p>Não perca essas ofertas que estão no site.</p>"
 					+ "<p>é por tempo limitado! Não vai perder essa chance, ein?!</p>"
 					+ "<a href=\"http://localhost:8080/api/swagger-ui/index.html#/\">Clique aqui para ver essas ofertas </a>"
-					+ "<p>Atenciosamente Grupo 4.</p>" 
-					+ "</div>" 
-					+ "</body>\r\n"
-					+ "</html>\r\n");
+					+ "<p>Atenciosamente Grupo 4.</p>" + "</div>" + "</body>\r\n" + "</html>\r\n");
 
 			helper.setText(builder.toString(), true);
 
@@ -278,25 +264,13 @@ public class EmailService {
 			String nome = usuario.getNome();
 			helper.setSubject("Olá " + nome + " vamos recuperar a sua senha");
 			StringBuilder builder = new StringBuilder();
-			builder.append("<html>\r\n" 
-					+ "<body>\r\n" 
-					+ "" 
+			builder.append("<html>\r\n" + "<body>\r\n" + "" + "<div align=\"center\">\r\n"
+					+ "<h1>Recuperação de senha</h1>\r\n" + "</div>\r\n" + "<br/>\r\n" + ""
+					+ "<div align=\"center\">\r\n" + "<img src=\"cid:logo\">" + "</div>\r\n" + ""
 					+ "<div align=\"center\">\r\n"
-					+ "<h1>Recuperação de senha</h1>\r\n" 
-					+ "</div>\r\n" 
-					+ "<br/>\r\n" 
-					+ ""
-					+ "<div align=\"center\">\r\n" 
-					+ "<img src=\"cid:logo\">" 
-					+ "</div>\r\n"
-					+ "" 
-					+ "<div align=\"center\">\r\n" 
 					+ "<p>Se vocé pediu a redefinição de senha<a href=\"http://localhost:8080/api/swagger-ui/index.html#/\"> clique aqui</a>.</p>"
 					+ "<p>Se vocé não reconhece essa requisição ignore esse email.</p>"
-					+ "<p>Atenciosamente Grupo 4.</p>" 
-					+ "</div>" 
-					+ "</body>\r\n"
-					+ "</html>\r\n");
+					+ "<p>Atenciosamente Grupo 4.</p>" + "</div>" + "</body>\r\n" + "</html>\r\n");
 
 			helper.setText(builder.toString(), true);
 
@@ -320,25 +294,13 @@ public class EmailService {
 			String nome = usuario.getNome();
 			helper.setSubject("Olá " + nome + " vamos recuperar o sua conta");
 			StringBuilder builder = new StringBuilder();
-			builder.append("<html>\r\n" 
-					+ "<body>\r\n" 
-					+ "" 
+			builder.append("<html>\r\n" + "<body>\r\n" + "" + "<div align=\"center\">\r\n"
+					+ "<h1>Recuperação de conta</h1>\r\n" + "</div>\r\n" + "<br/>\r\n" + ""
+					+ "<div align=\"center\">\r\n" + "<img src=\"cid:logo\">" + "</div>\r\n" + ""
 					+ "<div align=\"center\">\r\n"
-					+ "<h1>Recuperação de conta</h1>\r\n" 
-					+ "</div>\r\n" 
-					+ "<br/>\r\n" 
-					+ ""
-					+ "<div align=\"center\">\r\n" 
-					+ "<img src=\"cid:logo\">" 
-					+ "</div>\r\n"
-					+ "" 
-					+ "<div align=\"center\">\r\n" 
 					+ "<p>Se vocé estáá tentando recuperar sua conta, <a href=\"http://localhost:8080/api/swagger-ui/index.html#/\">clique aqui</a> para ver o e-mail cadastrado.</p>"
 					+ "<p>Se vocé não reconhece essa requisição ignore esse email.</p>"
-					+ "<p>Atenciosamente Grupo 4.</p>" 
-					+ "</div>" 
-					+ "</body>\r\n"
-					+ "</html>\r\n");
+					+ "<p>Atenciosamente Grupo 4.</p>" + "</div>" + "</body>\r\n" + "</html>\r\n");
 
 			helper.setText(builder.toString(), true);
 
